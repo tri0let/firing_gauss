c8f048612d908659bdc14cb1b45b2fe1ee7bc23f
tri0let <thomas.e.robinson.v@gmail.com>
2026-03-26 11:46:28 -0400

    Added new fit function

--- analysis_playground.py
+++ analysis_playground.py
@@ -10,8 +10,18 @@ import numpy as np
 
 dict = PeakForces('csv_data/03-24-2026_magneti_pull_data[force_timex11].csv', included_runs=[2, 3, 4, 5, 6], plot=False, prominence=0.15)
 dict1 = PeakForces('csv_data/03-24-2026_magnetic_pull_data2[force_timex10].csv', included_runs=[12, 13, 14, 15, 16])
+dict0 = PeakForces('csv_data/3-25-2026__magnetic_pull_data[force_timex65].csv', included_runs=[51])
 
-force = dict['mean']
+peak_forces0 = dict0['forces'][0:8] + dict0['forces'][10:12]
+
+force0 = [np.mean(peak_forces0)]
+
+err0 = [np.std(peak_forces0)/len(peak_forces0)]
+
+
+force = force0 + dict['mean']
+
+print(force)
 
 force1 = dict1['mean']
 
@@ -19,6 +29,8 @@ err = []
 
 for e, n in zip(dict['error'], dict['noise']):
     err.append(max(e, n))
+
+err = err0 + err
 
 err1 = []
 
@@ -28,7 +40,7 @@ for e, n in zip(dict1['error'], dict1['noise']):
 # for t, r in zip(dict['times'], dict['runs']):
 #     print(f'Peak times for run {r}:    {t}')
 
-dist = np.array([0.35, 0.67, 1.29, 1.93, 2.57])/1000
+dist = np.array([0.05, 0.35, 0.67, 1.29, 1.93, 2.57])
 
 # fastplot(dict['raw_data'][0]['time'], dict['raw_data'][0]['force'])
 
@@ -37,20 +49,25 @@ n = 1
 def f_1(x, a, h):
     return a / (x - h)**n
 
-params, param_err, y_fit, residuals, chi2, chi2_red = fit(model=f_1, x=dist, y=force, yerr=err, label='blue')
+def f_2(z, C, a, h):
+    return C * (((z - h + 1.27) / np.sqrt((z - h + 1.27)**2 + a**2))-((z - h - 1.27) / np.sqrt((z - h - 1.27)**2 + a**2)))
+
+params, param_err, y_fit, residuals, chi2, chi2_red = fit(model=f_2, x=dist, y=force, yerr=err, label='blue')
 
 print(params)
 print(param_err)
 print(chi2)
 
 fig, ax = plt.subplots()
-ax.errorbar(np.array(dist), np.array(force), yerr=err, fmt='o', label='One magnet')
-ax.errorbar(np.array(dist), np.array(force1), yerr=err1, fmt='o', label='Two magnets')
+ax.errorbar(dist, np.array(force), yerr=err, fmt='o', label='One magnet')
+ax.plot(dist, y_fit, label = 'Predicted fit')
+# ax.errorbar(np.array(dist), np.array(force1), yerr=err1, fmt='o', label='Two magnets')
 
-for n in range(1, 12):
-    params, param_err, y_fit, residuals, chi2, chi2_red = fit(model=f_1, x=dist, y=force, yerr=err, label='blue')
-    print(f'n = {n}: chi2 = {chi2}')
-    ax.plot(dist, y_fit, label=f'1/x^{n} fit')
+# for n in range(1, 12):
+#     params, param_err, y_fit, residuals, chi2, chi2_red = fit(model=f_1, x=dist, y=force, yerr=err, label='blue')
+#     print(f'n = {n}: reduced chi squared = {chi2_red}')
+#     print(f'h: {params['h']}')
+#     ax.plot(dist, y_fit, label=f'1/x^{n} fit')
 
 ax.set_xlabel('Distance to magnet surface (m)')
 ax.set_ylabel('Force (N)')
