31598e2bbb0b6103117b62a61f797fb39121ed30
Rose23-2 <jenylmarie23@gmail.com>
2026-04-08 18:16:15 -0400

    Refactor data loading and processing in analysis script

--- GaussianCannonAnalysis.py
+++ GaussianCannonAnalysis.py
@@ -1,23 +1,31 @@
-import os
+from pathlib import Path
 import pandas as pd
 import numpy as np
 from scipy.interpolate import UnivariateSpline
 import matplotlib.pyplot as plt
+import os
 
 # ========= LOAD ==========
 folder = "csv_data"
-g1 = pd.read_csv(os.path.join(folder, "gaussian_cannon_4-01-2026_summary.csv"))
-g_all = pd.read_csv(os.path.join(folder, "gaussian_cannon_summary.csv"))
+
+
+g_all = pd.read_csv(os.path.join(folder, "gaussian_cannon_all_stations_summary.csv"))
 nm = pd.read_csv(os.path.join(folder, "no_magnets_summary.csv"))
 
-# File structure
-for df in [g1, g_all, nm]:
-    df["mean_speed"] = pd.to_numeric(df["mean_speed"], errors="coerce")
-    df["sem_speed"] = pd.to_numeric(df["sem_speed"], errors="coerce")
-    df["stations"] = pd.to_numeric(df["stations"], errors="coerce")
-    df["distance"] = pd.to_numeric(df["distance"], errors="coerce")
-    df["magnets"] = pd.to_numeric(df["magnets"], errors="coerce")
-    df["group"] = df["group"].astype(str).str.strip()
+# File structrure
+for df in [ g_all, nm]:
+    if "mean_speed" in df.columns:
+        df["mean_speed"] = pd.to_numeric(df["mean_speed"], errors="coerce")
+    if "sem_speed" in df.columns:
+        df["sem_speed"] = pd.to_numeric(df["sem_speed"], errors="coerce")
+    if "stations" in df.columns:
+        df["stations"] = pd.to_numeric(df["stations"], errors="coerce")
+    if "distance" in df.columns:
+        df["distance"] = pd.to_numeric(df["distance"], errors="coerce")
+    if "magnets" in df.columns:
+        df["magnets"] = pd.to_numeric(df["magnets"], errors="coerce")
+    if "group" in df.columns:
+        df["group"] = df["group"].astype(str).str.strip()
 
 # ========= SPLIT ENTRANCE VELOCITY / EXIT VELOCITY ==========
 def split(df):
@@ -32,11 +40,11 @@ def split(df):
     )
     return merged
 
-g1 = split(g1)
+
 g_all = split(g_all)
 nm = split(nm)
 
-full = pd.concat([g1, g_all], ignore_index=True)
+full = pd.concat([g_all], ignore_index=True)
 
 # ========= REMOVE RUN 162-171 ==========
 full = full[full["group"].str.strip() != "Run 162-171"].copy()
@@ -209,4 +217,5 @@ for i, s in enumerate(stations_to_plot):
 plt.tight_layout()
 plt.show()
 plt.close()
+ 
 
